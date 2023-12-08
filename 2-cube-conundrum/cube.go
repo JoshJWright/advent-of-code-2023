58f5ebc0d11243e029cc1bb849b7f2ea8e61c562
Josh Wright <j.wright3141@outlook.com>
2023-12-08 00:03:55 +0000

    Day 2 Part 1 complete

---
+++ 2-cube-conundrum/cube.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Game struct {
+	id   int
+	info []CubeRecord
+}
+
+type CubeRecord struct {
+	red   int
+	green int
+	blue  int
+}
+
+func main() {
+	rawInput, _ := os.ReadFile("input.txt")
+	input := string(rawInput)
+	games := parseGames(input)
+
+	limit := CubeRecord{red: 12, green: 13, blue: 14}
+	var sum int
+	for _, game := range games {
+		if game.isWithin(limit) {
+			sum += game.id
+		}
+	}
+	fmt.Println(sum)
+}
+
+func (game Game) isWithin(limit CubeRecord) bool {
+	for _, record := range game.info {
+		if record.red > limit.red || record.green > limit.green || record.blue > limit.blue {
+			return false
+		}
+	}
+	return true
+}
+
+func parseGames(input string) []Game {
+	gameData := strings.Split(input, "\n")
+
+	games := make([]Game, 0, len(gameData))
+
+	var gameId *regexp.Regexp = regexp.MustCompile(`Game (\d+): `)
+
+	for _, data := range gameData {
+		idMatch := gameId.FindStringSubmatch(data)
+		id, _ := strconv.Atoi(idMatch[1])
+		cubeSets := strings.Split(data[len(idMatch[0]):], `;`)
+
+		game := Game{id: id, info: make([]CubeRecord, len(cubeSets))}
+
+		var cubes *regexp.Regexp = regexp.MustCompile(`(\d+) (\w+)`)
+		for i, cubeSet := range cubeSets {
+			parsedCubes := cubes.FindAllStringSubmatch(cubeSet, -1)
+			for _, colour := range parsedCubes {
+				num, _ := strconv.Atoi(colour[1])
+				switch strings.ToLower(colour[2]) {
+				case "red":
+					game.info[i].red = game.info[i].red + num
+				case "blue":
+					game.info[i].blue = game.info[i].blue + num
+				case "green":
+					game.info[i].green = game.info[i].green + num
+				}
+			}
+		}
+		games = append(games, game)
+	}
+	return games
+}
