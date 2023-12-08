68f1eb2531db2e08e58450985c1e493e192e8c4a
Josh Wright <j.wright3141@outlook.com>
2023-12-08 00:15:53 +0000

    Day 2 Part 2 complete

--- 2-cube-conundrum/cube.go
+++ 2-cube-conundrum/cube.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -24,12 +25,14 @@ func main() {
 	input := string(rawInput)
 	games := parseGames(input)
 
-	limit := CubeRecord{red: 12, green: 13, blue: 14}
+	// limit := CubeRecord{red: 12, green: 13, blue: 14}
 	var sum int
 	for _, game := range games {
-		if game.isWithin(limit) {
-			sum += game.id
-		}
+		// if game.isWithin(limit) {
+		// 	sum += game.id
+		// }
+
+		sum += game.computePower()
 	}
 	fmt.Println(sum)
 }
@@ -41,6 +44,18 @@ func (game Game) isWithin(limit CubeRecord) bool {
 		}
 	}
 	return true
+}
+
+func (game Game) computePower() int {
+	minimums := CubeRecord{}
+	for _, record := range game.info {
+		minimums = CubeRecord{
+			red:   int(math.Max(float64(minimums.red), float64(record.red))),
+			green: int(math.Max(float64(minimums.green), float64(record.green))),
+			blue:  int(math.Max(float64(minimums.blue), float64(record.blue))),
+		}
+	}
+	return minimums.red * minimums.green * minimums.blue
 }
 
 func parseGames(input string) []Game {
